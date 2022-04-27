3e2433a8d924a2a7188f180ca0bc6b341c359e8e
HJ <83521479+zodia8393@users.noreply.github.com>
2022-04-27 21:19:49 +0900

    Create SNA_2.R

---
+++ SNA_2.R
@@ -0,0 +1,49 @@
+getwd()
+setwd('C:/Users/User/Documents/WS') #집 컴퓨터용 디렉터리
+
+library(RSelenium)
+library(wdman)
+
+RSelenium_Connect=function(){
+  tryCatch(expr={
+    port_no=as.integer(sample(2000:8000,1))
+    rs=chrome(port=port_no)
+    remDr<-remoteDriver(remoteServerAddr="localhost",
+                        port=port_no,
+                        browserName="chrome")
+    remDr$open()
+  },finally={
+    port_no=as.integer(sample(2000:8000,1))
+    browser_ver=binman::list_versions(appname = "chromedriver")
+    browser_ver=browser_ver[[1]][length(browser_ver[[1]])-2]
+    rs=chrome(port=port_no,version=browser_ver)
+    remDr<-remoteDriver(remoteServerAddr="localhost",
+                        port=port_no,
+                        browserName="chrome")
+    remDr$open()
+    remDr$navigate('https://www.instagram.com/')
+    Sys.sleep(2)
+    
+    #아이디 비번
+    id<-remDr$findElement(using="xpath",value='//*[@id="loginForm"]/div/div[1]/div/label/input')
+    pw<-remDr$findElement(using="xpath",value='//*[@id="loginForm"]/div/div[2]/div/label/input')
+    #로그인 버튼
+    login_btn<-remDr$findElement(using="xpath",value='//*[@id="loginForm"]/div/div[3]/button/div')
+
+    id$clickElement()
+    Sys.sleep(1)
+    id$sendKeysToElement(list("testaccount00118"))
+    Sys.sleep(1)
+    pw$clickElement()
+    Sys.sleep(1)
+    pw$sendKeysToElement(list("1q2w3e4r1!"))
+    Sys.sleep(1)
+    login_btn$clickElement()
+  })
+}
+
+RSelenium_Connect()
+
+
+#현재 진행상황 RSelenium으로 크롬 열고 인스타 로그인까지 완료
+
